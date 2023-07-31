67d62befd7a1191156d976e0998db22f3ce40688
Mustafa Karadeniz <mkaraden@student.42istanbul.com.tr>
2023-07-31 19:17:16 +0300

    added ex06

---
+++ ex06/Harl.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkaraden <mkaraden@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/31 18:53:46 by mkaraden          #+#    #+#             */
+/*   Updated: 2023/07/31 19:16:18 by mkaraden         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+	
+}
+Harl::~Harl()
+{
+	
+}
+
+void Harl::complain( std::string level )
+{
+
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	switch (std::find(levels, levels + 4, level) - levels)
+	{
+	case 0:
+		this->debug();
+	case 1:
+		this->info();
+	case 2:
+		this->warning();
+	case 3:
+		this->error();
+	
+	default:
+		break;
+	}
+	
+}
+
+void Harl::debug( void )
+{
+	std::cout << "debug" << std::endl;
+}
+void Harl::info( void )
+{
+	std::cout << "info" << std::endl;
+}
+void Harl::warning( void )
+{
+	std::cout << "warning" << std::endl;
+}
+void Harl::error( void )
+{
+	std::cout << "error" << std::endl;
+}
